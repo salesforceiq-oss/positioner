59daeb2e114531cf25d78c92931a68fb0c7f1a81
Sterling Camden <sterling@relateiq.com>
2014-11-20 11:38:47 -0800

    abstract tools and basic matchers

---
+++ basic-matchers.js
@@ -0,0 +1,87 @@
+(function () {
+
+    function makeFakePosRange(t, l, r, b) {
+        return {top: t, left: l, right: r, bottom: b};
+    }
+
+    function maybeAddPx(v) {
+        return typeof v === 'string' ? v : v + 'px';
+    }
+
+
+    var $ = require('jquery');
+    var tools = require('./index.js');
+
+    var matchers = {
+
+        toBeANumber: tools.defineBasicMatcher(function (actual) {
+            return angular.isNumber(actual);
+        }),
+        toBeAFunction: tools.defineBasicMatcher(function (actual) {
+            return angular.isFunction(actual);
+        }),
+
+        toBeAnObject: tools.defineBasicMatcher(function (actual) {
+            return angular.isObject(actual);
+        }),
+
+        toBeAnArray: tools.defineBasicMatcher(function (actual) {
+            return angular.isArray(actual);
+        }),
+        toBeAString: tools.defineBasicMatcher(function (actual) {
+            return angular.isString(actual);
+        }),
+        toBeNully: tools.defineBasicMatcher(function (actual) {
+            return actual === undefined || actual === null;
+        }),
+        toBeAnElement: tools.defineBasicMatcher(function (actual) {
+            return !!(actual &&
+                (actual.nodeName || // we are a direct element
+                    (actual.prop && actual.attr && actual.find)));
+        }),
+        toHaveField: tools.defineBasicMatcher(function (actual, exp) {
+            return exp in actual;
+        }, function (actual, exp, pass) {
+            return tools.expectedObjectWithNot(actual, pass) + ' to have field: ' + exp;
+        }),
+        toContainAll: tools.defineBasicMatcher(function (actual, array) {
+            array.forEach(function (item) {
+                if (actual.indexOf(item) === -1) {
+                    return false;
+                }
+            });
+            return true;
+        }),
+        toHaveClass: tools.defineBasicMatcher(function (actual, className) {
+            return $(actual).hasClass(className);
+        }, function (actual, expected, pass) {
+            return 'Expected "' + $(actual).attr('class') + '"' + (pass ? ' not' : '') + ' to have class "' + expected + '"';
+        }),
+        toBePositioned: function () {
+            return {
+                compare: function (actual, t, l, b, r) {
+                    var top = $(actual).css('top');
+                    var left = $(actual).css('left');
+                    var right = $(actual).css('right');
+                    var bottom = $(actual).css('bottom');
+                    var pos = $(actual).css('position');
+                    var pass = top === maybeAddPx(t) &&
+                        left === maybeAddPx(l) &&
+                        right === maybeAddPx(r) &&
+                        bottom === maybeAddPx(b) &&
+                        pos === 'absolute';
+                    return {
+                        pass: pass,
+                        message: tools.expectedObjectWithNot(actual, pass, makeFakePosRange(top, left, right, bottom)) + ' to be positioned ' + JSON.stringify(makeFakePosRange(t, l, r, b))
+                    };
+                }
+            };
+        }
+
+    };
+
+    beforeEach(function () {
+        jasmine.addMatchers(matchers);
+    });
+})
+();
---
+++ index.js
@@ -0,0 +1,24 @@
+exports.defineBasicMatcher =
+    function defineBasicMatcher(passFn, messageFn) {
+        return function () {
+            return {
+                compare: function (actual, expected) {
+                    var pass = passFn(actual, expected);
+                    return {
+                        pass: pass,
+                        message: messageFn && messageFn(actual, expected, pass)
+                    };
+                }
+            };
+        };
+    };
+
+exports.expectedObjectWithNot = function expectedObjectWithNot(actual, pass, obj) {
+    try {
+        actual = JSON.stringify(obj || actual);
+    } catch (e) {
+        actual = 'actual';
+    }
+    return 'Expected ' + actual + (pass ? '' : ' not' );
+};
+
