2ac0942180dd2e8ba4a7d8754dc0bcb141626137
Sterling Camden <sterling@relateiq.com>
2015-05-15 12:09:38 -0700

    fix karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -6,24 +6,7 @@ module.exports = function(config) {
 
         basePath: '',
 
-        files: [
-            global.PUBLIC_WEBAPP_PATH + 'bower_components/jquery/jquery.js',
-            global.PUBLIC_WEBAPP_PATH + 'bower_components/numeral/numeral.js',
-            global.PUBLIC_WEBAPP_PATH + 'bower_components/sugar/release/sugar-full.min.js',
-            global.PUBLIC_WEBAPP_PATH + 'js/riq-utils.js',
-            'node_modules/angular/angular.js',
-            'node_modules/angular-ui-router/release/angular-ui-router.js',
-            'node_modules/angular-sanitize/angular-sanitize.js',
-            'node_modules/angular-cookies/angular-cookies.js',
-            'node_modules/angular-mocks/angular-mocks.js',
-            global.PUBLIC_WEBAPP_PATH + 'bower_components/angular-ui/build/angular-ui.js',
-            global.PUBLIC_WEBAPP_PATH + 'js/thrift.js',
-            global.PUBLIC_WEBAPP_PATH + 'js/thrift-riq.js',
-            global.PUBLIC_WEBAPP_PATH + 'js/*.js',
-            global.PUBLIC_WEBAPP_PATH + 'js/angular-app/test/helpers/jasmine-jquery.js',
-            global.PUBLIC_WEBAPP_PATH + 'js/angular-app/!(test)/**/!(*@(app|.spec)).js',
-            global.PUBLIC_WEBAPP_PATH + 'js/angular-app/templates.js'
-        ],
+        files: [],
 
         autoWatch: true,
 
