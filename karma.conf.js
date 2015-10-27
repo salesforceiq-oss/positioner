d9d93420823fb5bbe71cc31b7c1ee783d4490a32
Joe Chrzanowski <joechrz@gmail.com>
2015-10-27 14:11:11 -0700

    unnecessary

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,3 @@
-global.PUBLIC_WEBAPP_PATH = global.PUBLIC_WEBAPP_PATH || 'src/main/webapp/public/';
 module.exports = function(config) {
     // store the karma config in a JSON file to make it easy to change w/jq
     var contents = require('fs').readFileSync('karma.conf.json');
